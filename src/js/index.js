c1c71abb26712f5b5e16c4247519b3c8a1065d0d
kwizl <karis.njoroge@gmail.com>
2020-08-20 20:52:45 +0300

    Reverse changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,8 @@
-import { Detail } from './views/detailView';
+// import { Display } from './views/display.js';
 import Search from './models/search';
-import { elements } from './controller/base';
 
-function Init() {
-  const city = elements.inputName.value;
-  const search = new Search(String(city));
-  const detail = Detail();
-
-  elements.submit.addEventListener('submit', (e) => {
-    e.preventDefault();
-    search.getResults();
-    detail.result();
-  });
-
-  const data = search.getResults();
-  elements.name.textContent = data.name;
-  elements.temp.textContent = data.main.temp;
-  elements.desc.textContent = data[0].description;
-  elements.humidity.textContent = data.humidity;
-  elements.wind.textContent = data.wind;
-}
-
-Init();
+const city = document.querySelector('.inputName').value;
+const search = new Search(String(city));
+document.querySelector('.inputSubmit').addEventListener('click', () => {
+  search.getResults();
+});
--- src/js/models/search.js
+++ src/js/models/search.js
@@ -3,14 +3,13 @@ export default class Search {
     this.query = query;
   }
 
-  // eslint-disable-next-line consistent-return
   async getResults() {
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.query}&appid=5b2f48f1d0bbdc18d1a72fba0922419e`, { mode: 'cors' });
       this.result = await response.json();
-      return this.result;
+      console.log(this.result);
     } catch (error) {
-      return error.message;
+      alert(error);
     }
   }
 }
