01454b296bbfd4b5531b19339b0f81be0f45958a
kwizl <karis.njoroge@gmail.com>
2020-08-21 01:46:54 +0300

    Append api data to DOM

--- src/js/models/search.js
+++ src/js/models/search.js
@@ -12,11 +12,11 @@ export default class Search {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.query}&appid=5b2f48f1d0bbdc18d1a72fba0922419e`, { mode: 'cors' });
       const json = await response.json();
       display.result();
-      document.querySelector('.name').textContent = json.name;
-      document.querySelector('.temp').textContent = json.main.temp;
-      document.querySelector('.desc').textContent = json.weather[0].description;
-      document.querySelector('.humidity').textContent = json.main.humidity;
-      document.querySelector('.wind').textContent = json.wind.speed;
+      document.querySelector('.name').textContent = `City: ${json.name}`;
+      document.querySelector('.temp').textContent = `Temp: ${json.main.temp} C`;
+      document.querySelector('.desc').textContent = `Desc: ${json.weather[0].description}`;
+      document.querySelector('.humidity').textContent = `Humidity: ${json.main.humidity}`;
+      document.querySelector('.wind').textContent = `Wind Speed: ${json.wind.speed}`;
       console.log(json);
     } catch (error) {
       console.log(error.message);
