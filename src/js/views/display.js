157ff055e1897cb71de8a8c4eb04998e961c2616
kwizl <karis.njoroge@gmail.com>
2020-08-30 13:28:52 +0300

    Add image to describe weather

--- src/js/views/display.js
+++ src/js/views/display.js
@@ -6,29 +6,25 @@ const Display = () => {
     const p1 = document.createElement('p');
     const p2 = document.createElement('p');
     const p3 = document.createElement('p');
-    const p4 = document.createElement('p');
 
     divResults.classList.add('results');
     h2.classList.add('name');
     p1.classList.add('temp');
     p2.classList.add('desc');
     p3.classList.add('humidity');
-    p4.classList.add('wind');
 
     content.appendChild(divResults);
     divResults.appendChild(h2);
     divResults.appendChild(p1);
     divResults.appendChild(p2);
     divResults.appendChild(p3);
-    divResults.appendChild(p4);
   };
 
   const apiData = (data) => {
     document.querySelector('.name').textContent = `City: ${data.name}`;
     document.querySelector('.temp').textContent = `Temp: ${data.main.temp} °C`;
     document.querySelector('.desc').textContent = `Description: ${data.weather[0].description}`;
-    document.querySelector('.humidity').textContent = `Humidity: ${data.main.humidity}%`;
-    document.querySelector('.wind').textContent = `Wind Speed: ${data.wind.speed} m/s`;
+    document.querySelector('.humidity').innerHTML = `<img src=" http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>`;
   };
 
   const resetData = () => {
@@ -36,7 +32,6 @@ const Display = () => {
     document.querySelector('.temp').textContent = '';
     document.querySelector('.desc').textContent = '';
     document.querySelector('.humidity').textContent = '';
-    document.querySelector('.wind').textContent = '';
   };
 
   const errorDisplay = (error) => {
